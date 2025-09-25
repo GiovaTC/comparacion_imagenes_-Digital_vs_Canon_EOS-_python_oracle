0c88f16548f28940c071ade50e5ce732c769df6c
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-25 14:27:00 -0500

    tercer - commit :. .

--- image_compare_oracle.py
+++ image_compare_oracle.py
@@ -11,4 +11,13 @@ import matplotlib.pyplot as plt
 import cv2
 from skimage.metrics import structural_similarity as ssim
 
-#cargar .env    
+#cargar .env
+load_dotenv()
+
+ORACLE_USER = os.getenv("ORACLE_USER")
+ORACLE_PASS = os.getenv("ORACLE_PASS")
+ORACLE_DSN = os.getenv("ORACLE_DSN") # por ejemplo :"localhost:puerto/orclpdb1"
+POOL_MIN = int(os.getenv("ORACLE_POOL_MIN", "1"))
+POOL_MAX = int(os.getenv("ORACLE_POOL_MAX", "4"))
+
+#rutas por defecto ( puedes cambiar ) .
